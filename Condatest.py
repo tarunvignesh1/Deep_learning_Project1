05217cd52fc3d0a3f2bc828a60563260646cfc3c
TarunStark <84522189+tarunvignesh1@users.noreply.github.com>
2021-07-04 21:47:41 +0530

    checking cuda avialability

--- Condatest.py
+++ Condatest.py
@@ -2,3 +2,9 @@ import torch as t
 x = t.rand(5, 3)
 print(x)
 
+train_on_gpu = t.cuda.is_available()
+
+if not train_on_gpu:
+    print('CUDA is not available.  Training on CPU ...')
+else:
+    print('CUDA is available!  Training on GPU ...')
